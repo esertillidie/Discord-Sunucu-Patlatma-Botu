a21284b2d61ffd6f4029749e671ae1973950cad9
Eser <qtapperr@gmail.com>
2024-08-15 19:31:56 +0300

    Add files via upload

---
+++ Başlat.bat
@@ -0,0 +1,6 @@
+@echo off
+title E5ERAURA Discord Sunucu Siken Bot
+cls
+:a
+node index.js
+goto a
---
+++ commands/server.js
@@ -0,0 +1,12 @@
+module.exports.run = async (client, message, args) => {
+
+message.guild.setName("E5ERAURA & STAFFEN FAST S3X")
+message.guild.setIcon("https://i.hizliresim.com/dfjpi4j.png")
+
+}
+
+module.exports.help = {
+
+    name: "server"
+
+}
